3241ce98bc4c5dfb90a5f5825201fbc94b5e5058
Luca <61409445+melusc@users.noreply.github.com>
2021-12-21 21:57:03 +0100

    export operation-to-string

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {generateTable, type ParsedTable} from './generate-table.js';
 export {parseOperation, type AST} from './parse-operation.js';
 export * from './logical-symbols.js';
+export {operationToString} from './operation-to-string.js';
