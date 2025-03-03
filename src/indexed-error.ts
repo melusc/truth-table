7d8a2320eb4e6fb6ed1c5a800f3b38811bfc6712
Luca Schnellmann <oss@lusc.ch>
2025-03-03 12:43:13 +0100

    feat(indexed-error): Make `from` and `to` readonly and rewrite for potential future `erasableSyntaxOnly`

--- src/indexed-error.ts
+++ src/indexed-error.ts
@@ -1,11 +1,12 @@
 export class IndexedError extends Error {
 	public override readonly name = 'IndexedError';
+	public readonly from: number;
+	public readonly to: number;
 
-	constructor(
-		errorMessage: string,
-		public from: number,
-		public to: number,
-	) {
+	constructor(errorMessage: string, from: number, to: number) {
 		super(errorMessage);
+
+		this.from = from;
+		this.to = to;
 	}
 }
